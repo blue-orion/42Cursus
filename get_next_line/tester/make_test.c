6ac4a1e55fbbc0c536d854aa308fd77f981df719
UnivTaeho <xogh080907@gmail.com>
2024-10-24 01:15:09 +0900

    1024

--- get_next_line/tester/make_test.c
+++ get_next_line/tester/make_test.c
@@ -6,6 +6,11 @@ int	main(int argc, char *argv[])
 {
 	char	command[100];
 
+	if (argc > 2)
+	{
+		printf("Invalid argc");
+		return (0);
+	}
 	if (argc == 1)
 	{
 		strcpy(command, "cc ");
@@ -14,16 +19,18 @@ int	main(int argc, char *argv[])
 	}
 	else
 	{
-		strcpy(command, "cc -D BUFFER_SIZE=");
+		strcpy(command, "cc ");
+		// strcat(command, "-Wall -Wextra -Werror ");
+		strcat(command, "-D BUFFER_SIZE=");
 		strcat(command, argv[1]);
-		strcat(command, " ../get_next_line.c ../get_next_line_utils.c test.c -o test.out");
+		strcat(command, " ../get_next_line.c ../get_next_line_utils.c test.c ");
+		strcat(command, "-o test.out");
 	}
 	printf("%s\n", command);
 	int	ret = system(command);
 	
 	if (ret != 0)
 		printf("ret : %d \n", ret);
-	
 	// test.out 실행
 	if (ret == 0)
 	{
