47e378c0b553ce01ee38acf519986f86124cfa56
Taeho <taeho080907@gmail.com>
2024-10-28 21:25:39 +0900

    Mandatory

--- get_next_line/tester/make_test.c
+++ get_next_line/tester/make_test.c
@@ -4,7 +4,7 @@
 
 int	main(int argc, char *argv[])
 {
-	char	command[100];
+	char	command[1000];
 
 	if (argc > 2)
 	{
@@ -19,8 +19,8 @@ int	main(int argc, char *argv[])
 	}
 	else
 	{
-		strcpy(command, "cc ");
-		// strcat(command, "-Wall -Wextra -Werror ");
+		strcpy(command, "cc -g ");
+		strcat(command, "-Wall -Wextra -Werror ");
 		strcat(command, "-D BUFFER_SIZE=");
 		strcat(command, argv[1]);
 		strcat(command, " ../get_next_line.c ../get_next_line_utils.c test.c ");
@@ -41,18 +41,69 @@ int	main(int argc, char *argv[])
 	}
 	if (ret == 0)
 	{
-		printf("rm test.out\n");
-		ret = system("rm test.out");
+		printf("valgrind --leak-check=full ./test.out\n");
+		printf("===============================================================================\n\n");
+		ret = system("valgrind --leak-check=full ./test.out");
+		printf("\n===============================================================================\n");
 	}
+	// if (ret == 0)
+	// {
+	// 	printf("rm test.out\n");
+	// 	ret = system("rm test.out");
+	// }
 	if (ret == 0)
 	{
-		printf("diff text.txt text_user.txt\n");
+		printf("diff -c -d text.txt text_user.txt\n");
 		printf("===============================================================================\n\n");
-		ret = system("diff -q text.txt text_user.txt");
+		ret = system("diff -c -d text.txt text_user.txt");
 		if (ret == 0)
 			printf("diff OK!\n");
 		else
 			system("diff text.txt text_user.txt");
+		printf("\n===============================================================================\n\n");
+	}
+	if (ret == 0)
+	{
+		printf("diff -c -d empty.txt empty_user.txt\n");
+		printf("===============================================================================\n\n");
+		ret = system("diff -c -d empty.txt empty_user.txt");
+		if (ret == 0)
+			printf("diff OK!\n");
+		else
+			system("diff empty.txt empty_user.txt");
+		printf("\n===============================================================================\n\n");
+	}
+	if (ret == 0)
+	{
+		printf("diff -c -d read_error.txt read_error_user.txt\n");
+		printf("===============================================================================\n\n");
+		ret = system("diff -c -d read_error.txt read_error_user.txt");
+		if (ret == 0)
+			printf("diff OK!\n");
+		else
+			system("diff read_error.txt read_error_user.txt");
+		printf("\n===============================================================================\n\n");
+	}
+	if (ret == 0)
+	{
+		printf("diff -c -d big_line.txt big_line_user.txt\n");
+		printf("===============================================================================\n\n");
+		ret = system("diff -c -d big_line.txt big_line_user.txt");
+		if (ret == 0)
+			printf("diff OK!\n");
+		else
+			system("diff big_line.txt big_line_user.txt");
+		printf("\n===============================================================================\n\n");
+	}
+	if (ret == 0)
+	{
+		printf("diff -c -d 41_no_nl.txt 41_no_nl_user.txt\n");
+		printf("===============================================================================\n\n");
+		ret = system("diff -c -d 41_no_nl.txt 41_no_nl_user.txt");
+		if (ret == 0)
+			printf("diff OK!\n");
+		else
+			system("diff 41_no_nl.txt 41_no_nl_user.txt");
 		printf("\n===============================================================================\n\n");
 	}
 	return (0);
