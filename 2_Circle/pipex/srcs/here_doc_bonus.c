59670fe96222a825d28d8a6e1aa4e13934df4061
Taeho <taeho080907@gmail.com>
2024-12-15 18:19:51 +0900

    here_doc end strcmp

--- 2_Circle/pipex/srcs/here_doc_bonus.c
+++ 2_Circle/pipex/srcs/here_doc_bonus.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   here_doc.c                                         :+:      :+:    :+:   */
+/*   here_doc_bonus.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: takwak <takwak@student.42gyeongsan.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/13 05:16:48 by takwak            #+#    #+#             */
-/*   Updated: 2024/12/13 05:16:48 by takwak           ###   ########.fr       */
+/*   Updated: 2024/12/15 18:17:43 by takwak           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,11 +32,14 @@ void	here_doc_input(char *limiter, t_pipex *info)
 {
 	char	*line;
 
+	limiter = ft_strjoin(limiter, "\n");
+	if (!limiter)
+		return ;
 	write(1, "pipe heredoc> ", 14);
 	line = get_next_line(0);
 	while (line)
 	{
-		if (!ft_strncmp(line, limiter, ft_strlen(limiter)))
+		if (!ft_strncmp(line, limiter, ft_strlen(limiter) + 1))
 			break ;
 		write(info->p1[1], line, ft_strlen(line));
 		free(line);
@@ -44,5 +47,6 @@ void	here_doc_input(char *limiter, t_pipex *info)
 		line = get_next_line(0);
 	}
 	free(line);
+	free(limiter);
 	close(info->p1[1]);
 }
