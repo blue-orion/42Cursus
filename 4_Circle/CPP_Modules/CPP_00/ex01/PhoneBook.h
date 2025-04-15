3b606d32fbcd11b4370ed55c608a48291510be54
blue-orion <xogh080907@gmail.com>
2025-04-15 23:50:54 +0900

    doing cpp00, ex01

--- 4_Circle/CPP_Modules/CPP_00/ex01/PhoneBook.h
+++ 4_Circle/CPP_Modules/CPP_00/ex01/PhoneBook.h
@@ -6,7 +6,7 @@
 /*   By: takwak <takwak@student.42gyoengsan.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/12 21:47:11 by takwak            #+#    #+#             */
-/*   Updated: 2025/04/15 19:35:41 by takwak           ###   ########.fr       */
+/*   Updated: 2025/04/15 23:49:44 by takwak           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,89 @@
 
 # include "Contact.h"
 # include <iostream>
+# include <iomanip>
 
 class PhoneBook {
 private:
 	Contact contact[8];
-	int		num;
+	int		size;
 
 public:
+	PhoneBook() {
+		size = 0;
+	}
+
+	void	add_contact(void)
+	{
+		std::string	input;
+
+		std::cout << "input first name" << std::endl;
+		std::cin >> input;
+		contact[size].set_first_name(input);
+
+		std::cout << "input last name" << std::endl;
+		std::cin >> input;
+		contact[size].set_last_name(input);
+
+		std::cout << "input nickname" << std::endl;
+		std::cin >> input;
+		contact[size].set_nickname(input);
+
+		std::cout << "input phone number" << std::endl;
+		std::cin >> input;
+		contact[size].set_phone_number(input);
+
+		std::cout << "input darkest secret" << std::endl;
+		std::cin >> input;
+		contact[size].set_darkest_secret(input);
+
+		this->size++;
+	}
+
+	void	search_contact(void)
+	{
+		std::string	tmp;
+
+		for (int i = 0; i < size; i++) {
+			std::cout << std::right << std::setw(10) << i + 1 << "|";
+			
+			tmp = contact[i].get_first_name();
+			if (tmp.length() > 10)
+				tmp = tmp.substr(0, 9) + ".";
+			std::cout << std::right << std::setw(10) << tmp << "|";
+			
+			tmp = contact[i].get_last_name();
+			if (tmp.length() > 10)
+				tmp = tmp.substr(0, 9) + ".";
+			std::cout << std::right << std::setw(10) << tmp << "|";
+
+			tmp = contact[i].get_nickname();
+			if (tmp.length() > 10)
+				tmp = tmp.substr(0, 9) + ".";
+			std::cout << std::right << std::setw(10) << tmp << std::endl;
+		}
+
+		int	index;
+
+		std::cout << "Enter index : ";
+		std::cin >> index;
+		
+		if (index - 1 > size) {
+			std::cout << "Not Exist" << std::endl;
+		}
+		else {
+			std::cout << index << std::endl
+				<< "first name : " << contact[index - 1].get_first_name() << std::endl
+				<< "last name : " << contact[index - 1].get_last_name() << std::endl
+				<< "nickname : " << contact[index - 1].get_nickname() << std::endl
+				<< "phone number : " << contact[index - 1].get_phone_number() << std::endl
+				<< "darkest secret : " << contact[index - 1].get_darkest_secret() << std::endl;
+		}
+	}
+
+	int	get_size(void) {
+		return (this->size);
+	}
 };
 
 #endif
--- 4_Circle/CPP_Modules/CPP_00/ex01/phonebook.cpp
+++ 4_Circle/CPP_Modules/CPP_00/ex01/phonebook.cpp
@@ -6,7 +6,7 @@
 /*   By: takwak <takwak@student.42gyoengsan.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/12 18:55:39 by takwak            #+#    #+#             */
-/*   Updated: 2025/04/12 22:03:32 by takwak           ###   ########.fr       */
+/*   Updated: 2025/04/15 22:44:02 by takwak           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,28 @@
 
 int	main(void)
 {
-	PhoneBook	phone_book[8];
+	PhoneBook	phone_book;
+	std::string	input;
 
-
+	while (1)
+	{
+		std::cin >> input;
+		if (!input.compare("ADD"))
+		{
+			phone_book.add_contact();
+			std::cout << input << std::endl;
+		}
+		else if (!input.compare("SEARCH"))
+		{
+			phone_book.search_contact();
+			std::cout << input << std::endl;
+		}
+		else if (!input.compare("EXIT"))
+		{
+			std::cout << input << std::endl;
+			break ;
+		}
+		else
+			std::cout << "invalid input" << std::endl;
+	}
 }
