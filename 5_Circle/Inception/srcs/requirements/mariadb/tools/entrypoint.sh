7bf7d26f06389eedebe7f968c29b39fefba210de
blue-orion <xogh080907@gmail.com>
2025-06-10 00:42:59 +0900

    Inception: https in local

--- 5_Circle/Inception/srcs/requirements/mariadb/tools/entrypoint.sh
+++ 5_Circle/Inception/srcs/requirements/mariadb/tools/entrypoint.sh
@@ -1,16 +1,12 @@
 #!/bin/sh
 
-# ✅ 초기화 필요 시 시스템 테이블 생성
 if [ ! -d /var/lib/mysql/mysql ]; then
-    echo "[INFO] Initializing MariaDB data directory..."
     mysql_install_db --user=mysql --basedir=/usr --datadir=/var/lib/mysql
 
-    echo "[INFO] Starting temporary MariaDB to run init.sql..."
     mysqld_safe --skip-networking &
     sleep 5
 
-    echo "[INFO] Creating init.sql..."
-    mysql --protocol=socket -u root <<EOF
+    mysql --protocol=socket -u root << EOF
 CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\` CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
 CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
 GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO '${MYSQL_USER}'@'%';
@@ -21,9 +17,7 @@ EOF
     sleep 2
 fi
 
-# ✅ 퍼미션 정리
 chown -R mysql:mysql /var/lib/mysql
 
-# ✅ 포그라운드로 진짜 MariaDB 실행
 exec mysqld_safe
 
--- 5_Circle/Inception/srcs/requirements/wordpress/tools/entrypoint.sh
+++ 5_Circle/Inception/srcs/requirements/wordpress/tools/entrypoint.sh
@@ -1,43 +1,25 @@
 #!/bin/sh
 
 CONFIG_PATH=/var/www/html/wp-config.php
+sleep 10
 
-# # ✅ MariaDB가 연결될 때까지 대기
-# echo "[INFO] Waiting for MariaDB to be ready..."
-# while ! ping "${DB_HOST%%:*}"; do
-# 	sleep 1
-# done
-
-# ✅ wp-config.php 생성
 if [ ! -f "$CONFIG_PATH" ]; then
 	echo "[INFO] Generating wp-config.php..."
-	cat > "$CONFIG_PATH" <<EOF
-<?php
-define('DB_NAME', '${MYSQL_DATABASE}');
-define('DB_USER', '${MYSQL_USER}');
-define('DB_PASSWORD', '${MYSQL_PASSWORD}');
-define('DB_HOST', '${DB_HOST}');
-define('DB_CHARSET', 'utf8');
-define('DB_COLLATE', '');
-
-define('AUTH_KEY',         '$(openssl rand -base64 32)');
-define('SECURE_AUTH_KEY',  '$(openssl rand -base64 32)');
-define('LOGGED_IN_KEY',    '$(openssl rand -base64 32)');
-define('NONCE_KEY',        '$(openssl rand -base64 32)');
-define('AUTH_SALT',        '$(openssl rand -base64 32)');
-define('SECURE_AUTH_SALT', '$(openssl rand -base64 32)');
-define('LOGGED_IN_SALT',   '$(openssl rand -base64 32)');
-define('NONCE_SALT',       '$(openssl rand -base64 32)');
-
-\$table_prefix = 'wp_';
-define('WP_DEBUG', false);
-
-if ( !defined('ABSPATH') )
-	define('ABSPATH', dirname(__FILE__) . '/');
-require_once(ABSPATH . 'wp-settings.php');
-EOF
+	wp config create	\
+		--dbname=$MYSQL_DATABASE	\
+		--dbuser=$MYSQL_USER		\
+		--dbpass=$MYSQL_PASSWORD	\
+		--dbhost=$DB_HOST
 fi
 
-# ✅ php-fpm 실행
+echo "[INFO] Install wordpress..."
+wp core install \
+	--url=localhost:8443	\
+	--title=Inception 		\
+	--admin_user=wpuser 	\
+	--admin_password=wppassword \
+	--admin_email=info@wp-cli.org
+
+
 exec php-fpm82 -F
 
